b460a2f673b1f4ed73aef0bc71e91fc0f94878e7
cin-vong <61393238+cin-vong@users.noreply.github.com>
2021-01-03 00:38:00 +0100

    commit 05

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,3 @@
-
-
 let nameValue = document.getElementById('name').value;
 let emailValue = document.getElementById('email').value;
 
@@ -23,12 +21,10 @@ async function queryPost(url) {
     }
     
     let reponse = await fetch (url, options);
-    let data = await response.json();
+    let data = await reponse.json();
     console.log(data);
     return data;
 }
-
-
 
 //Signup
 const btn = document.getElementById('btn');
@@ -38,3 +34,49 @@ btn.addEventListener('click', e => {
     queryPost('http://localhost:3000/api/auth/signup');
 })
 
+//Affichage
+
+const listeReponse = [
+    {
+    'name': nameValue,
+    'address': emailValue,
+    }
+  ]
+  
+
+  function displayData(data) {
+    const listeBox = document.getElementById('liste');
+    
+    const title = document.createElement('h1');
+    title.textContent = 'Liste: ';
+    listeBox.appendChild(title)
+    
+    for ( const people in data ) {
+      console.log (data[people])
+      let person = data[people];
+      
+      const article = document.createElement('article');
+      listeBox.appendChild(article)
+      
+      const name = document.createElement('h2');
+    name.textContent = person.name;
+    article.appendChild(name)
+      
+       const infos = document.createElement('p');
+    infos.textContent = `Email: ${person.emailValue} Nom: ${person.nameValue}`;
+    article.appendChild(infos) 
+
+    const btnA = document.getElementById('btnA');
+    btnA.addEventListener('click', e => {
+        e.preventDefault();
+        queryPost('http://localhost:3000/api/auth');
+    })
+
+    }
+  }
+   
+  displayData(listeReponse)
+  
+  
+    
+    
