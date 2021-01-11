0b483d26c4cd430ff14e12d66d94d611d03109fb
cin-vong <61393238+cin-vong@users.noreply.github.com>
2021-01-11 11:01:02 +0100

    commit-07

--- backend/controllers/list.js
+++ backend/controllers/list.js
@@ -3,6 +3,17 @@ const mysql = require('mysql');
 
 const con = mysql.createConnection(dbParams);
 
+exports.GetoneList = (req, res, next) => {
+    con.connect(function(err) {
+        if (err) throw err;
+           con.query("SELECT * FROM customers", function (err, result, fields) {
+         if (err) throw err;
+             console.log(result);
+             });
+        });
+    }
+
+
 exports.GetallList = (req, res, next) => {
          con.connect(function(err) {
              if (err) throw err;
--- backend/routes/list.js
+++ backend/routes/list.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const listCtrl = require('../controllers/list');
 
-router.get('/list', listCtrl.GetallList);
+router.get('/user', listCtrl.GetoneList);
+router.post('/user', listCtrl.GetallList);
 
 module.exports = router;
